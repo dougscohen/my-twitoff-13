00f46247d267e19dc4a5a0673d9f260dd50b67b5
dougscohen <dougscohen@gmail.com>
2020-04-20 12:54:02 -0400

    created flask app

---
+++ hello.py
@@ -0,0 +1,14 @@
+# hello.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/about")
+def about():
+    x = 2 + 2
+    return f"about me {x}"
