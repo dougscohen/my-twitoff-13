5a169e6675b55097d94fd78dadfb388ff0a9e9cb
dougscohen <dougscohen@gmail.com>
2020-04-21 13:44:30 -0400

    added twitter service

---
+++ web_app/services/twitter_service.py
@@ -0,0 +1,49 @@
+# web_app/services/twitter_service.py
+
+import tweepy
+import os
+from dotenv import load_dotenv
+from pprint import pprint
+
+load_dotenv()
+
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+# def twitter_api():
+auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+print("AUTH", auth) #> <tweepy.api.API object at 0x110899790>
+
+api = tweepy.API(auth)
+print("API", api) #> <tweepy.api.API object at 0x110899790>
+#print(dir(api))
+    # return api
+
+# if __name__ == "__main__":
+
+    # api = twitter_api()
+user = api.get_user("elonmusk")
+print("---------------")
+print("USER", user) #> <class 'tweepy.models.User'>
+print(user.screen_name)
+print(user.name)
+print(user.followers_count)
+pprint(user._json)
+
+print("--------------")
+statuses = api.user_timeline("elonmusk")
+pprint(statuses[0]._json)
+
+
+    #breakpoint()
+
+    #public_tweets = api.home_timeline()
+    #
+    #for tweet in public_tweets:
+    #    print(type(tweet)) #> <class 'tweepy.models.Status'>
+    #    #print(dir(tweet))
+    #    print(tweet.text)
+    #    print("-------------")
