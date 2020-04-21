5e860a26c80901e1828ac30da54a3d191357a8de
dougscohen <dougscohen@gmail.com>
2020-04-21 18:07:23 -0400

    storing users in the database

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,6 +1,7 @@
 # web_app/routes/twitter_routes.py
 
 from flask import Blueprint, jsonify
+from web_app.models import User, Tweet, db
 from web_app.services.twitter_service import twitter_api
 
 twitter_routes = Blueprint("twitter_routes", __name__)
@@ -9,13 +10,19 @@ twitter_routes = Blueprint("twitter_routes", __name__)
 def get_user(screen_name=None):
     print(screen_name)
     api = twitter_api()
-    user = api.get_user(screen_name)
+    twitter_user = api.get_user(screen_name)
     statuses = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+    print("STATUSES COUNT:", len(statuses))
     
     # store them in the database
+    db_user = User.query.get(twitter_user.id) or User(id=twitter_user.id)
+    db_user.screen_name = twitter_user.screen_name
+    db_user.name = twitter_user.name
+    db_user.location = twitter_user.location
+    db_user.followers_count = twitter_user.followers_count
+    db.session.add(db_user)
+    db.session.commit() 
     
     
-    
-    
-    return jsonify({"user": user._json, "tweets_count": len(statuses)})
+    return "OK"
 
