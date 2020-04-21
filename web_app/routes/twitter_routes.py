058cc5b9d0538c1d8dd987d548b452ef944951ae
dougscohen <dougscohen@gmail.com>
2020-04-21 18:28:17 -0400

    storing tweets in the database

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, jsonify
 from web_app.models import User, Tweet, db
 from web_app.services.twitter_service import twitter_api
+from web_app.services.basilica_service import connection as basilica_connection
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
@@ -14,7 +15,7 @@ def get_user(screen_name=None):
     statuses = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
     print("STATUSES COUNT:", len(statuses))
     
-    # store them in the database
+    # store users in the database
     db_user = User.query.get(twitter_user.id) or User(id=twitter_user.id)
     db_user.screen_name = twitter_user.screen_name
     db_user.name = twitter_user.name
@@ -22,7 +23,28 @@ def get_user(screen_name=None):
     db_user.followers_count = twitter_user.followers_count
     db.session.add(db_user)
     db.session.commit() 
-    
+
+
+    # store tweets in the database
+    all_tweet_texts = [status.full_text for status in statuses]
+    embeddings = list(basilica_connection.embed_sentences(all_tweet_texts, model="twitter"))
+    print("NUMBER OF EMBEDDINGS:", len(embeddings))
+
+    counter = 0
+    for status in statuses:
+        print(status.full_text)
+        print('----')
+
+        db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
+        db_tweet.user_id = status.author.id
+        db_tweet.full_text = status.full_text
+
+        embedding = embeddings[counter]
+        print(len(embedding))
+        db_tweet.embedding = embedding
+        db.session.add(db_tweet)
+        counter+=1
+    db.session.commit()
     
     return "OK"
 
--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -13,28 +13,25 @@ connection = basilica.Connection(API_KEY)
 print(type(connection)) #> <class 'basilica.Connection'>
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     print("---------")
-#     connection = basilica_api_client()
-
-print("---------")
-sentence = "Hello again"
-sent_embeddings = connection.embed_sentence(sentence)
-print(list(sent_embeddings))
+    print("---------")
+    sentence = "Hello again"
+    sent_embeddings = connection.embed_sentence(sentence)
+    print(list(sent_embeddings))
 
 
-print("---------")
-sentences = ["Hello world!", "How are you?"]
-print(sentences)
-# it is more efficient to make a single request for all sentences...
-embeddings = connection.embed_sentences(sentences)
-print("EMBEDDINGS...")
-print(type(embeddings))
-print(list(embeddings)) # [[0.8556405305862427, ...], ...]
+    print("---------")
+    sentences = ["Hello world!", "How are you?"]
+    print(sentences)
+    # it is more efficient to make a single request for all sentences...
+    embeddings = connection.embed_sentences(sentences)
+    print("EMBEDDINGS...")
+    print(type(embeddings))
+    print(list(embeddings)) # [[0.8556405305862427, ...], ...]
 
-# You can request based on a 'twitter' sentence
-print("---------")
-tweet_text = "I love #ArtificialIntelligence"
-tweet_embedding = connection.embed_sentence(tweet_text, model="twitter")
-print(list(tweet_embedding))
+    # You can request based on a 'twitter' sentence
+    print("---------")
+    tweet_text = "I love #ArtificialIntelligence"
+    tweet_embedding = connection.embed_sentence(tweet_text, model="twitter")
+    print(list(tweet_embedding))
