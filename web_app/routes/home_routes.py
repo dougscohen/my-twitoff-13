e2f78fc112e512ae08bfc153f5198181415386d6
dougscohen <dougscohen@gmail.com>
2020-04-22 14:19:35 -0400

    implented Logistic Regression model

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -4,9 +4,9 @@ from flask import Blueprint, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
-@home_routes.route("/")
-def index():
-    return "Hello Twitoff!"
+# @home_routes.route("/")
+# def index():
+#     return "Hello Twitoff!"
 
 @home_routes.route("/about")
 def about():
