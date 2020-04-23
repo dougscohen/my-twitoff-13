bce9013dd20280d007b20ee9e3f58b55e6424dd2
dougscohen <dougscohen@gmail.com>
2020-04-23 16:02:36 -0400

    configure DB in production

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,7 @@
 # web_app/__init__.py
 
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 from web_app.models import db, migrate
@@ -9,12 +11,16 @@ from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
 from web_app.routes.stats_routes import stats_routes
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 # application factory pattern
 def create_app():
     app = Flask(__name__)
 
     # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_tweets.db"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users\\dougcohen\\Repos\\Unit-3\\my-twitoff-13\\web_app\\web_app_tweets.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     db.init_app(app)
     migrate.init_app(app, db)
 
